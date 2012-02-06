07c146eeeb8e18a7139f7cf510b2158673627342
root <root@d3v11.ano>
2012-02-06 05:41:57 +0000

    bugfixes

--- hub.py
+++ hub.py
@@ -21,17 +21,13 @@ switch={}
 
 while 1:
     this_packet,this_client=hub.recvfrom(65536)
-    these_KVPs=udpmsg4.unframe(this_packet)
     md5sum=md5(this_packet).digest()
 
     if not md5sum in md5cache:
         md5cache+=[md5sum]
-
-        if these_KVPs!=0:
-            if 'CMD' in these_KVPs:
-                for this_socket in os.listdir(remotesockdir):
-                    if this_socket!=this_client:
-                        try:
-                            hub.sendto(this_packet,remotesockdir+'/'+this_socket)
-                        except:
-                            pass
+        for this_socket in os.listdir(remotesockdir):
+            if this_socket!=this_client:
+                try:
+                    hub.sendto(this_packet,remotesockdir+'/'+this_socket)
+                except:
+                    pass
