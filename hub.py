b0a2deeab0b9c7ab479c4e9cfb30c5413ab15dd3
Nick <nick@somerandomnick.ano>
2012-02-10 18:18:07 +0000

    bugfixes from c.udpmsg4.srn.ano

--- hub.py
+++ hub.py
@@ -10,7 +10,8 @@ import udpmsg4
 
 hubsock=(sys.argv[1])
 remotesockdir=sys.argv[2]
-os.write(2,'hub.py start '+remotesockdir+'\n')
+pid=str(os.getpid())
+os.write(2,'hub.py '+pid+' start '+remotesockdir+'\n')
 hub=socket.socket(socket.AF_UNIX,socket.SOCK_DGRAM)
 try:
     hub.bind(hubsock)
@@ -26,7 +27,7 @@ queue=dict()
 sha512cache=[]
 while 1:
     message=''
-    message+='hub.py SUCCESS ['
+    message+='hub.py '+pid+' SUCCESS ['
     for this_socket in success:
         message+=this_socket+','
     message+=']'
@@ -45,23 +46,34 @@ while 1:
         message+=','
     message+=']\n'
     os.write(2,message+'\n')
+    queued=0
     for key in queue.keys():
         if len(queue[key])>0:
+            queued+=1
             try:
-                write_length=hub.sendto(queue[key][0],key)
+                write_length=hub.sendto(queue[key][0],remotesockdir+'/'+key)
                 if write_length!=len(queue[key][0]):
-                    os.write(2,'hub.py error: write_length == '+str(write_length)+', packet_length == '+str(len(queue[key][0]))+'\n')
+                    os.write(2,'hub.py '+pid+' error: write_length == '+str(write_length)+', packet_length == '+str(len(queue[key][0]))+'\n')
                 queue[key].popleft()
-                os.write(2,'hub.py extra send success '+key+' (still '+str(len(queue[key]))+')\n')
+                os.write(2,'hub.py '+pid+' extra send success '+key+' (still '+str(len(queue[key]))+')\n')
+                queued+=100
             except socket.error, ex:
                 if ex.errno == 111:
-                    os.write(2,'hub.py socket dead '+key+'('+str(len(queue[key]))+')\n')
-                    os.remove(key)
+                    os.write(2,'hub.py '+pid+' socket dead '+key+'('+str(len(queue[key]))+')\n')
+                    try:
+                        os.remove(remotesockdir+'/'+key)
+                    except:
+                        pass
                 if ex.errno != 11:
-                    os.write(2,'hub.py error: cannot write to '+key+' '+str(ex.errno)+'\n')
+                    os.write(2,'hub.py '+pid+' error: cannot write to '+key+' '+str(ex.errno)+'\n')
                 if ex.errno == 11:
-                    os.write(2,'hub.py error: try again write to '+key+' (still '+str(len(queue[key]))+')\n')
-    read_this=readable([hubfd],[],[],1)[0]
+                    os.write(2,'hub.py '+pid+' error: try again write to '+key+' (still '+str(len(queue[key]))+')\n')
+    if queued>0:
+        timeout=1.0/queued
+    else:
+        timeout=1
+    os.write(2,'hub.py '+pid+' poll timeout '+str(timeout)+'\n')
+    read_this=readable([hubfd],[],[],timeout)[0]
     clientsocketpaths=os.listdir(remotesockdir)
     message=''
     for key in clientsocketpaths:
@@ -69,12 +81,12 @@ while 1:
             queue[key]=collections.deque([],128)
             message+=os.path.basename(key)+','
     if len(message)>0:
-        message='hub.py NEW ['+message+']\n'
+        message='hub.py '+pid+' NEW ['+message+']\n'
         os.write(2,message+'\n')
     if hubfd in read_this:
 
         this_packet,this_client=hub.recvfrom(65536)
-        os.write(2,'hub.py received from '+this_client+'\n')
+        os.write(2,'hub.py '+pid+' received from '+this_client+'\n')
         sha512sum=sha512(this_packet).digest()
 
         if not sha512sum in sha512cache:
@@ -88,18 +100,19 @@ while 1:
                         try:
                             write_length=hub.sendto(this_packet,remotesockdir+'/'+this_socket)
                             if write_length!=packet_length:
-                                os.write(2,'hub.py error: write_length == '+str(write_length)+', packet_length == '+str(packet_length)+'\n')
+                                os.write(2,'hub.py '+pid+' error: write_length == '+str(write_length)+', packet_length == '+str(packet_length)+'\n')
+                            os.write(2,'hub.py '+pid+' success: can write to '+this_socket+'\n')
                             success+=[this_socket]
                         except socket.error, ex:
                             if ex.errno == 111:
-                                os.write(2,'hub.py socket dead '+remotesockdir+'/'+this_socket+'\n')
+                                os.write(2,'hub.py '+pid+' socket dead '+remotesockdir+'/'+this_socket+'\n')
                                 os.remove(remotesockdir+'/'+this_socket)
                             if ex.errno != 11:
-                                os.write(2,'hub.py error: cannot write to '+remotesockdir+'/'+this_socket+' '+str(ex.errno)+'\n')
+                                os.write(2,'hub.py '+pid+' error: cannot write to '+remotesockdir+'/'+this_socket+' '+str(ex.errno)+'\n')
                             if ex.errno == 11:
                                 eagain+=[this_socket]
                                 queue[this_socket].append(this_packet)
                     else:
                         queue[this_socket].append(this_packet)
                 else:
-                    os.write(2,'hub.py received from '+this_socket+'\n')
+                    os.write(2,'hub.py '+pid+' received from '+this_socket+'\n')
--- stream.py
+++ stream.py
@@ -7,7 +7,7 @@ readable=select.select
 
 pathhub=(sys.argv[1])
 pid=str(os.getpid())
-os.write(2,'stream.py '+pid+' starting\n')
+os.write(2,'stream.py '+pid+' starting '+os.getenv('TCPREMOTEIP')+':'+os.getenv('TCPREMOTEPORT')+'\n')
 pathclient=sys.argv[2]+'/'+pid
 client=socket.socket(socket.AF_UNIX,socket.SOCK_DGRAM)
 
@@ -27,11 +27,19 @@ while 1:
 #    sockets=readable([6,peerfd],[7,clientfd],[],1)
 #    read_this=sockets[0]
 #    write_this=sockets[1]
+    os.write(2,'stream.py '+pid+' start poll for read\n')
     read_this=readable([0,clientfd],[],[],1)[0]
-    write_this=readable([],[1,clientfd],[],1)[1]
+    readables=''
+    for readabless in read_this:
+        readables+=str(readabless)+','
+    os.write(2,'stream.py '+pid+' end poll for read '+str(len(readables))+'['+readables+']\n')
+    os.write(2,'stream.py '+pid+' start poll for write\n')
+    write_this=readable([],[1,clientfd],[],0)[1]
+    os.write(2,'stream.py '+pid+' end poll for write\n')
     os.write(2,'stream.py '+pid+' start loop reads '+str(len(read_this))+' writes '+str(len(write_this))+'\n')
     if read_this!=[]:
         if clientfd in read_this:
+            os.write(2,'stream.py '+pid+' reading from clientfd\n')
             hub_packet=client.recv(65536)
             hub_packet=chr(int(round(len(hub_packet)/256)))+chr(int(round(len(hub_packet)%256)))+hub_packet
             if not hub_packet:
@@ -42,10 +50,18 @@ while 1:
                 write_length=0
                 packet_length=len(toremote)
                 while write_length!=packet_length:
-                    write_this=readable([],[1],[])[1]
+                    os.write(2,'stream.py '+pid+' try to write '+str(packet_length)+'\n')
+                    os.write(2,'stream.py '+pid+' in loop start poll for write\n')
+                    write_this=readable([],[1],[],0)[1]
+                    os.write(2,'stream.py '+pid+' in loop end poll for write '+str(len(write_this))+' writables\n')
+                    if len(write_this)==0:
+                        os.write(2,'stream.py '+pid+' in loop end poll no writables\n')
+                        break
                     if 1 in write_this:
                         try:
+                            os.write(2,'stream.py '+pid+' in loop start write\n')
                             write_length=os.write(1,toremote)
+                            os.write(2,'stream.py '+pid+' in loop end write\n')
                         except os.error, ex:
                             if ex.errno == 104:
                                 os.write(2,'stream.py '+pid+' connection closed from remote\n')
@@ -54,20 +70,28 @@ while 1:
                         if write_length>0:
                             toremote=toremote[write_length::]
                             packet_length=len(toremote)
+                        else:
+                            if len(toremote)>0:
+                                os.write(2,'stream.py '+pid+' zero length write\n')
+                                os.remove(pathclient)
+                                break
             except socket.error, ex:
                 os.write(2,'stream.py '+pid+' error: cannot write to '+pathclient+' '+str(ex.errno)+'\n')
                 os.remove(pathclient)
                 break
 
         if 0 in read_this:
+            os.write(2,'stream.py '+pid+' reading from stdin\n')
             try:
+                os.write(2,'stream.py '+pid+' start read\n')
                 client_packet=os.read(0,4096)
+                os.write(2,'stream.py '+pid+' end read\n')
                 if not client_packet:
                     os.remove(pathclient)
                     break
                 fromremote+=client_packet
-            except:
-                os.write(2,'stream.py '+pid+' error: udpmsg4 protocol error\n')
+            except os.error, ex:
+                os.write(2,'stream.py '+pid+' error: udpmsg4 protocol error '+str(ex.errno)+'\n')
                 try:
                     os.remove(pathclient)
                 except:
@@ -83,7 +107,9 @@ while 1:
                 fromremote=fromremote[packet_length::]
                 try:
                     packet_length=len(client_packet)
+                    os.write(2,'stream.py '+pid+' start write to hub\n')
                     write_length=client.sendto(client_packet,pathhub)
+                    os.write(2,'stream.py '+pid+' end write to hub\n')
                 except socket.error, ex:
                     os.write(2,'client.py '+pid+' error: cannot write to '+pathhub+' '+str(ex.errno)+'\n')
                 if len(fromremote)>=2:
